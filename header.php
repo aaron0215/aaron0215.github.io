3ca75707a827534627dcb082dc3ab4d88e95191a
aaron0215 <47953681+aaron0215@users.noreply.github.com>
2019-12-25 22:18:30 -0600

    Delete header.php

--- header.php
+++
@@ -1,7 +0,0 @@
-<!-- ######################     Page header   ############################## -->
-
-<header>
-    <div class="header">
-    <img src="photo/whitelogo.png" alt="logo" width="300" height="300" class="logo">
-    </div>
-</header>
